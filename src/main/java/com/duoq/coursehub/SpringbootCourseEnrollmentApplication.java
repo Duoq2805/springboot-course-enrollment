7a58b39124dc79abf61beab3e2890359061dd0f2
Duoq2805 <duongkt.st.2005@gmail.com>
2025-09-22 18:30:14 +0700

    First commit

---
+++ src/main/java/com/duoq/coursehub/SpringbootCourseEnrollmentApplication.java
@@ -0,0 +1,13 @@
+package com.duoq.coursehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCourseEnrollmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCourseEnrollmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/duoq/coursehub/SpringbootCourseEnrollmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoq.coursehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCourseEnrollmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
